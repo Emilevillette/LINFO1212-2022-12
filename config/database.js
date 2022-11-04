3e91bca62007479460ac9825e115898061130e47
Emile Villette <emile.villette@student.uclouvain.be>
2022-11-04 15:48:45 +0100

    renamed database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: "preparatoryproject.sqlite"
+    storage: "louevainlinux.sqlite"
 })
 
 module.exports = {sequelize};
