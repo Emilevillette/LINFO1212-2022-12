987be9bd73506f3a39daa2a41b51d71365806fd3
Emile Villette <emile.villette@student.uclouvain.be>
2022-11-25 16:43:16 +0100

    sequelize model

--- models/receipt.js
+++ models/receipt.js
@@ -2,21 +2,15 @@ const {DataTypes, Model} = require("sequelize");
 
 const {sequelize} = require("../config/database");
 
-class Recu extends Model {
+class Receipt extends Model {
 }
 
-Recu.init({
+Receipt.init({
     n_commande: {
         type: DataTypes.TEXT,
         allowNull: false,
         primaryKey: true
-    },
-    commande: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: Commandes,
-            key: 'commandeID'
-        }
     }
 }, ({sequelize}));
+
+module.exports = Receipt;
