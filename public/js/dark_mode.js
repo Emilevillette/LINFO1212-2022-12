5ada0df8738a8414dd4c445fb24a47e7ddcec909
xonouhaxo <80458621+xonouhaxo@users.noreply.github.com>
2022-12-16 15:16:28 +0100

    removed useless divs + better css

--- public/js/dark_mode.js
+++ public/js/dark_mode.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 
 //Check dark_mode cookie and turn it on if needed
@@ -29,6 +30,65 @@ function dark_mode() {
     }
     console.log("After " + document.cookie);
 }
+
+
+//sets minimum date in cart
+function min_date(){
+  var date_t = new Date();
+  var min_date_sortie = new Date(date_t.getTime() + 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
+  var date_retour = document.getElementById("date_retour");
+  var date_sortie = document.getElementById("date_sortie");
+
+  document.getElementById("date_sortie").min = min_date_sortie;
+  date_sortie.addEventListener("input", function () {
+      date_retour.disabled = false;
+      date_retour.min = date_sortie.value;
+  });
+}
+
+//generates pdf in cart
+function generatePDF() {
+  // eslint-disable-next-line no-undef
+  var pdf = new jsPDF;
+  var date_t = new Date();
+  var date = new Date(date_t.getTime() + 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
+  var img = new Image();
+  img.src = "img/llln.png";
+
+  pdf.addImage(img, "png", 14, 13, 10, 10);
+  pdf.setFontSize(30).setTextColor(47, 64, 109).text("LOUEvain-Li-Nux", 24, 22);
+  pdf.setFontSize(15).setFontStyle("italic").text("kot Louvain Linux", 160, 22);
+  pdf.setFontSize(13).setFontStyle("italic").text("Le: " + date, 170, 32);
+  pdf.setFontSize(16).setFontStyle("bold").text("La  commande que vous avez passé contient les éléments suivants:", 16, 90);
+  var table = document.getElementById("commande");
+  var b = 0;
+  var c = 120;
+
+  for (var i = 0; i < table.rows.length; i++) {
+      b+=1;
+   for (var j = 0; j < table.rows[i].cells.length; j++) {
+       var cellContent = table.rows[i].cells[j].innerHTML;
+       console.log(i);
+       console.log(table.rows[i].cells[j].innerHTML);
+      if (b==15){
+          pdf.addPage();
+          b = 1;
+          c = 20;
+      }
+      if (i<1){
+          pdf.setFontStyle("normal").text(cellContent + " :", 17+(j*50), 110);
+      } else if (i == 1){
+          pdf.setFontSize(10).setFontStyle("normal").text(cellContent, 20+(j*50), c+(b*10));
+      } else {
+          pdf.setFontStyle("normal").text(cellContent, 20+(j*50), c+(b*10));
+      }
+      
+   }}
+
+   pdf.save("reçu.pdf");
+}
+
+
 
 
 //Modify
